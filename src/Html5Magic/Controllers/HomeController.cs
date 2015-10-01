794668892f88cf8bd5869730bc0523633fcb45d3
juergengutsch <juergen@gutsch-online.de>
2015-10-01 08:47:34 +0200

    initial commit

---
+++ src/Html5Magic/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Html5Magic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Canvas()
+        {
+            return View();
+        }
+
+        public IActionResult Storage()
+        {
+            return View();
+        }
+
+        public IActionResult Location()
+        {
+            return View();
+        }
+
+        public IActionResult FileSystem()
+        {
+            return View();
+        }
+
+        public IActionResult DragDrop()
+        {
+            return View();
+        }
+
+        public IActionResult Camera()
+        {
+            return View();
+        }
+
+        public IActionResult MathML()
+        {
+            return View();
+        }
+
+        public IActionResult Svg()
+        {
+            return View();
+        }
+    }
+}
